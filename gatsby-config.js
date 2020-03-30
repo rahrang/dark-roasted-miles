0bc8806e141fe5b46aea3efad012480d98ea8a52
Rahul Rangnekar <rrangnekar96@gmail.com>
2020-03-29 18:42:02 -0700

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,17 +7,17 @@ module.exports = {
   ],
   // Customize your site metadata:
   siteMetadata: {
-    title: `My Blog Title`,
-    author: `My Name`,
+    title: `Dark Roasted Miles`,
+    author: `Kithumini Jayasiri & Rahul Rangnekar`,
     description: `My site description...`,
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/gatsbyjs`,
+        url: `https://twitter.com/darkroastmiles`,
       },
       {
         name: `github`,
-        url: `https://github.com/gatsbyjs`,
+        url: `https://github.com/rahrang`,
       },
     ],
   },
